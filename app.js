0d320632d542fe7466fdf4bb9ff7fd2bf912cf20
Carlos Baston <carlosbaston82@gmail.com>
2022-10-01 09:55:58 -0700

    data sanitize

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const expressValidator = require('express-validator')
 const morgan = require('morgan')
+const rateLimit = require('express-rate-limit')
+const helmet = require('helmet')
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
+const hpp = require('hpp')
 const userRouter = require('./routes/userRoutes')
 const weeklyResultsRouter = require('./routes/weeklyResultsRoutes')
 const filterRouter = require('./routes/filterRoutes')
@@ -13,10 +18,19 @@ const authRouter = require('./routes/authRoutes')
 const stripeController = require('./controllers/stripeController')
 
 const app = express()
-
+// SET SECURITY HTTP HEADERS
+app.use(helmet())
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
+
+// SET LIMIT REQUEST FROM SAME IP
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 100,
+    message: 'Too many requests from your IP, try again in an hour',
+})
+app.use('/api', limiter)
 
 // cannot run through jsonParser
 app.post(
@@ -25,9 +39,24 @@ app.post(
     stripeController.webhook,
 )
 
-app.use(express.json({ limit: '512MB' }))
+// BODY PARSER, TAKES PAYLOAD BODY AND PUTS IT IN REQ.BODY
+app.use(express.json({ limit: '10kb' }))
 app.use(expressValidator())
 
+// SANITIZE DATA AGAINST NOSQL QUERY INJECTION
+app.use(mongoSanitize())
+
+// SANITIZE  AGAINST XSS
+app.use(xss())
+
+// PREVENT PARAMETER POLLUTION
+app.use(
+    hpp({
+        whitelist: ['game'],
+    }),
+)
+
+// ROUTES
 app.use('/api/v1/yearly-events', yearlyEventRouter)
 app.use('/api/v1/weekly-events', weeklyEventRouter)
 app.use('/api/v1/auth', authRouter)
