f4af5dfd0c75072d256deb0d66b7dd38fb2c10d3
Carlos Baston <carlosbaston82@gmail.com>
2022-09-30 22:31:52 -0700

    create cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,6 +17,14 @@ const createToken = (_id) =>
 function sendToken(user, res, email) {
     const token = createToken(user._id)
 
+    const cookieOptions = {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+    }
+
+    if (process.env.NODE_ENV === 'production') cookieOptions.secure = true
+    res.cookie('jwt', token, cookieOptions)
+
     return res.status(200).json({
         fullName: user.fullName,
         email,
