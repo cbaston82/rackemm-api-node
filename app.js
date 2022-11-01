6903bc6dfc95b81d00e6a2d2773913de392e1751
Carlos Baston <carlosbaston82@gmail.com>
2022-11-01 16:24:18 -0700

    test cors

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const stripeController = require('./controllers/stripeController')
 
 const app = express()
 
-const whitelist = ['https://rackemm.netlify.app']
+const whitelist = ['https://rackemm.netlify.app', 'http:127.0.0.1']
 const corsOptions = {
     origin: function (origin, callback) {
         if (whitelist.indexOf(origin) !== -1) {
