a334f42a6697ead0ab5fc866695b996f2d875c05
Carlos Baston <carlosbaston82@gmail.com>
2022-11-02 14:39:01 -0700

    stripe webhook

--- app.js
+++ app.js
@@ -66,11 +66,7 @@ const limiter = rateLimit({
 app.use('/api', limiter)
 
 // cannot run through jsonParser
-app.post(
-    '/api/v1/stripe/webhook',
-    bodyParser.raw({ type: 'application/json' }),
-    stripeController.webhook,
-)
+app.post('/api/v1/stripe/webhook', bodyParser.raw({ type: '*/*' }), stripeController.webhook)
 
 // BODY PARSER, TAKES PAYLOAD BODY AND PUTS IT IN REQ.BODY
 app.use(express.json({ limit: '512MB' }))
