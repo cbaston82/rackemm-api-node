83fb07bc748b16a0a2adcdd42bf3d96a3ebc220f
Carlos Baston <carlosbaston82@gmail.com>
2022-11-23 14:18:41 -0800

    add stripe to cors

--- app.js
+++ app.js
@@ -21,41 +21,13 @@ const stripeController = require('./controllers/stripeController')
 
 const app = express()
 
-if (process.env.NODE_ENV === 'production') {
-    const whitelist = [
-        'www.rackemm.com',
-        'https://rackemm.com',
-        'https://www.rackemm.com',
-        'https://rackemm.netlify.app',
-    ]
-
-    const corsOptions = {
-        origin: function (origin, callback) {
-            if (whitelist.indexOf(origin) !== -1) {
-                callback(null, true)
-            } else {
-                callback(new Error('Not allowed by CORS'))
-            }
-        },
-        methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
-        optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-        credentials: true, //Credentials are cookies, authorization headers or TLS client certificates.
-        allowedHeaders: [
-            'Content-Type',
-            'Authorization',
-            'X-Requested-With',
-            'device-remember-token',
-            'Access-Control-Allow-Origin',
-            'Origin',
-            'Accept',
-        ],
-    }
-
-    app.use(cors(corsOptions))
-} else if (process.env.NODE_ENV !== 'production') {
-    app.use(cors())
-    app.use(morgan('dev'))
+const corsOptions = {
+    origin: '*',
+    credentials: true, //Credentials are cookies, authorization headers or TLS client certificates.
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
+
+app.use(cors(corsOptions))
 
 // cannot run through jsonParser
 app.post('/api/v1/stripe/webhook', express.raw({ type: '*/*' }), stripeController.webhook)
