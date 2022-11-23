99496bef9ee02025cab3971ab3a1cd292fb6c21c
Carlos Baston <carlosbaston82@gmail.com>
2022-11-23 13:45:28 -0800

    add stripe to cors

--- app.js
+++ app.js
@@ -25,9 +25,9 @@ const app = express()
 app.post('/api/v1/stripe/webhook', express.raw({ type: '*/*' }), stripeController.webhook)
 
 if (process.env.NODE_ENV === 'production') {
-    console.log('cors 2')
     app.use((req, res, next) => {
         res.header('Access-Control-Allow-Origin', 'https://www.rackemm.com') // update to match the domain you will make the request from
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
         res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
         next()
     })
